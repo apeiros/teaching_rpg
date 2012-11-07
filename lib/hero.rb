b424a2cb49123cdae5c7c3d7256540aed1e319f0
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-11-08 00:06:49 +0100

    Added armory to our hero.

--- lib/hero.rb
+++ lib/hero.rb
@@ -2,9 +2,16 @@
 
 # Our hero actors are represented by this class
 class Hero
+  attr_accessor :helmet, :gloves, :breastplate, :trousers, :boots
+
   def initialize(name)
     @name           = name
     @health_points  = 100   # every hero starts with 100 health points
+    @helmet         = 0     # initial armor equipment is 0
+    @gloves         = 0
+    @breastplate    = 0
+    @trousers       = 0
+    @boots          = 0
   end
 
   def hit(damage)
@@ -17,5 +24,9 @@ class Hero
   
   def alive?
     @health_points > 0
+  end
+
+  def armor
+    @helmet+@gloves+@breastplate+@trousers+@boots
   end
 end
