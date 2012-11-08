0d16801b613af9df1a2658034bb7bd7d6a184acd
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-11-08 22:28:17 +0100

    Added files to allow running the unit tests.

---
+++ test/lib/helper.rb
@@ -0,0 +1,33 @@
+module TestSuite
+  attr_accessor :name
+end
+
+module Kernel
+  def suite(name, &block)
+    klass = Class.new(Test::Unit::TestCase, &block)
+    klass.extend TestSuite
+    klass.name = "Suite #{name}"
+
+    klass
+  end
+  module_function :suite
+end
+
+class Test::Unit::TestCase
+  def self.test(desc, &impl)
+    define_method("test #{desc}", &impl)
+  end
+
+  def read_data(path, *options)
+    File.read(File.expand_path(path, TEST_DATA_DIR), *options)
+  end
+
+  def capture_stdout
+    captured  = StringIO.new
+    $stdout   = captured
+    yield
+    captured.string
+  ensure
+    $stdout = STDOUT
+  end
+end
