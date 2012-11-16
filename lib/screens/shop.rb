6e69f5aa71875910eba0162036f1f3ba9dce37e0
Stefan Rusterholz <stefan.rusterholz@gmail.com>
2012-11-16 14:50:59 +0100

    Simplified how row color is chosen.

--- lib/screens/shop.rb
+++ lib/screens/shop.rb
@@ -14,31 +14,29 @@ module Screens
 
     def rendered
       s=[]
-      
+
       s << hero_stats(@hero)
-     
-      @items.each_with_index do |item, index| 
+
+      @items.each_with_index do |item, index|
         if index == @cursor
-          if index.even?
-            s << ljust("\e[38;5;#{Black};48;5;#{Cyan}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", Cyan)
-          else 
-            s << ljust("\e[38;5;#{Black};48;5;#{White}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", White)
-          end
-        else       
-          if index.odd?
-            s << ljust("\e[38;5;#{Black};48;5;#{Cyan}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", Cyan)
-          else 
-            s << ljust("\e[38;5;#{Black};48;5;#{White}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", White)
-          end  
-        end  
-      end
-      
-      @items.size.upto(@max_items-1) do | index |
-        if index.odd?
-        s << ljust("\e[38;5;#{White};48;5;#{Cyan}m "*120 , Cyan)
+          row_color = 51
+        elsif index.even?
+          row_color = 251
         else
-        s  << ljust("\e[38;5;#{White};48;5;#{White}m "*120 , White)
-        end      
+          row_color = White
+        end
+        s << ljust("\e[38;5;#{Black};48;5;#{row_color}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", row_color)
+      end
+
+      @items.size.upto(@max_items-1) do | index |
+        if index == @cursor
+          row_color = 51
+        elsif index.even?
+          row_color = 251
+        else
+          row_color = White
+        end
+        s << ljust("\e[38;5;#{White};48;5;#{row_color}m "*120 , row_color)
       end
       if @items[@cursor]
         itemdesc= @items[@cursor].desc
@@ -47,10 +45,10 @@ module Screens
         s << ljust(" "*20+"#{lineone}", Black)
         s << ljust(" "*20+"#{linetwo}", Black)
       else
-        s << ljust("", Black)    
-        s << ljust("", Black)     
+        s << ljust("", Black)
+        s << ljust("", Black)
       end
-      
+
       s << help
       s.join
     end
@@ -62,6 +60,6 @@ module Screens
         'Q'     => 'Exit'
       )
     end
-   
+
   end
 end
