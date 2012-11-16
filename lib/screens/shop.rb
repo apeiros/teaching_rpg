b9c261547906f857f3a1b6e7068844cd4a9aca3b
Josh <josh@Josh-PC.(none)>
2012-11-16 13:28:35 +0000

    stuff

--- lib/screens/shop.rb
+++ lib/screens/shop.rb
@@ -9,6 +9,7 @@ module Screens
       @hero       = hero
       @items      = items
       @cursor     = 0
+      @max_items  = 36
     end
 
     def rendered
@@ -18,10 +19,10 @@ module Screens
      
       @items.each_with_index do |item, index| 
         if index == @cursor
-          if !index.odd?
-          s << ljust("\e[38;5;#{Black};48;5;#{Cyan}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", Cyan)
+          if index.even?
+            s << ljust("\e[38;5;#{Black};48;5;#{Cyan}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", Cyan)
           else 
-          s << ljust("\e[38;5;#{Black};48;5;#{White}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", White)
+            s << ljust("\e[38;5;#{Black};48;5;#{White}m "*20 + "     " + " "*(3-item.quantity.to_s.length) + "#{item.quantity}x "+" "*(12-item.type.length) +"#{item.type} "+" "*(16-item.name.length)+"#{item.name}"+"            Price: " + " "*(5-item.price.to_s.length) + "#{item.price}$", White)
           end
         else       
           if index.odd?
@@ -32,8 +33,8 @@ module Screens
         end  
       end
       
-      (36-counter).times do | x |
-        if (40-counter-x).even?
+      @items.size.upto(@max_items-1) do | index |
+        if index.odd?
         s << ljust("\e[38;5;#{White};48;5;#{Cyan}m "*120 , Cyan)
         else
         s  << ljust("\e[38;5;#{White};48;5;#{White}m "*120 , White)
@@ -41,18 +42,14 @@ module Screens
       end
       if @items[@cursor]
         itemdesc= @items[@cursor].desc
-        if itemdesc.split('').length > 80
-          lineone = itemdesc.split('')[0..79]
-          linetwo = itemdesc.split('')[80..(itemdesc.split('').length-1)]
-        else
-          lineone = itemdesc.split('')[0..(itemdesc.split('').length-1)]
-          linetwo = ''
+        lineone = itemdesc
+        linetwo = ''
         end
         s << ljust(" "*20+"#{lineone}", Black)
         s << ljust(" "*20+"#{linetwo}", Black)
       else
-      s << ljust("", Black)
-      s << ljust("", Black)      
+      s << ljust("", Black)    
+      s << ljust("", Black)     
       end
       
       s << help
