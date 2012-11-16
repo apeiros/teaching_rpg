1f7a5ae20bfe0c66a0810d4e03bc0c4865b91a5b
Josh <josh@Josh-PC.(none)>
2012-11-16 12:03:18 +0000

    stuff

--- lib/scenes/shopping.rb
+++ lib/scenes/shopping.rb
@@ -7,7 +7,7 @@ module Scenes
   class Shopping < Scene
     ShopItem = Struct.new(:type, :name, :quantity, :price, :desc)
     def initialize(game, items = [ShopItem.new('Use', 'Apple', 20, 5, 'A pretty green apple'), ShopItem.new('Helmet', 'Wool Cap', 1, 200, 'This is desc of wool cap') ] )
-      super()     
+      super    
       @screen = Screens::Shop.new(@game.hero, items )
     end
     
