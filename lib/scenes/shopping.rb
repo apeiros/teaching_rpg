6d615a3b7acd7ce0c309d69f4ec1f68a55eab35a
Josh <josh@Josh-PC.(none)>
2012-11-16 11:53:51 +0000

    stuff

--- lib/scenes/shopping.rb
+++ lib/scenes/shopping.rb
@@ -6,7 +6,7 @@ require 'scenes'
 module Scenes
   class Shopping < Scene
     ShopItem = Struct.new(:type, :name, :quantity, :price, :desc); ]
-    def initialize(game, items = [ShopItem.new('Use', 'Apple', 20, 5, 'A pretty green apple'), ShopItem.new('Helmet', 'Wool Cap', 1, 200, 'This is desc of wool cap'))
+    def initialize(game, items = [ShopItem.new('Use', 'Apple', 20, 5, 'A pretty green apple'), ShopItem.new('Helmet', 'Wool Cap', 1, 200, 'This is desc of wool cap') ] )
       super()     
       @screen = Screens::Shop.new(@game.hero, items )
     end
